3143aa2f076aae90b935e4ffa080af65198e9d6d
DenDev <Den17Dev@gmail.com>
2023-10-21 21:34:27 +0300

    Add Another class

---
+++ src/main/java/org/example/AnotherClass.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class AnotherClass {
+
+    private String text;
+
+    AnotherClass(){
+
+    }
+    AnotherClass(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
