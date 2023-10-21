00be8f8995376d80778f00770f8d2fd6472497cc
DenDev <Den17Dev@gmail.com>
2023-10-21 21:22:41 +0300

    Add TestClass

---
+++ src/main/java/org/example/TestClass.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class TestClass {
+
+    private int value;
+    TestClass(int value){
+        this.value = value;
+    }
+
+    TestClass(){
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
